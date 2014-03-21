c5e6a1ab0ed48804f614e05c6e7f521dfca6399f
Leon Waldman <lwaldman@uolinc.com>
2014-03-21 16:20:10 -0300

    Fixed indentation for best-practice/readability. Added usage function

--- cl-hltb.lisp
+++ cl-hltb.lisp
@@ -10,21 +10,44 @@
                 "cxml-stp"))
 
 ;;; \o/!
-(defun main()
-  (if (= (length *posix-argv*) 2)
-    (let* ((query (list (cons "queryString" (nth 1 *posix-argv*))))
+(defun print-usage()
+  (format t "Should pass on the game name.~C" #\newline))
+
+(defun fetch-and-print(game)
+    (let* ((query (list (cons "queryString" game)))
            (str (drakma:http-request "http://www.howlongtobeat.com/search_main.php?t=games&page=1&sorthead=&sortd=Normal&plat=&detail=0"
                                      :method :post
                                      :parameters query))
            (document (chtml:parse str (cxml-stp:make-builder))))
       (xpath:with-namespaces ((nil "http://www.w3.org/1999/xhtml"))
-        (let* ((gamename (xpath:string-value (xpath:evaluate "//h3/a" document)))
-               (mainstory (xpath:string-value (xpath:evaluate "//*[@class=\"gamelist_tidbit time_90\"]" document)))
-               (mainplusextra (xpath:string-value (xpath:evaluate "//*[@class=\"gamelist_tidbit time_70\"]" document)))
-               (completionist (xpath:string-value (xpath:evaluate "//*[@class=\"gamelist_tidbit time_60\"]" document)))
-               (combined (xpath:string-value (xpath:evaluate "//*[@class=\"gamelist_tidbit time_100\"]" document))))
-          (format t "Game Name: ~A~%Main Story: ~A~%Main + Extra: ~A~%Completionist: ~A~%Combined: ~A~%"
-                  gamename mainstory mainplusextra completionist combined))))))
+        (let* ((gamename
+                (xpath:string-value (xpath:evaluate
+                                     "//h3/a" document)))
+               (mainstory
+                (xpath:string-value (xpath:evaluate
+                                     "//*[@class=\"gamelist_tidbit time_90\"]" document)))
+               (mainplusextra
+                (xpath:string-value (xpath:evaluate
+                                     "//*[@class=\"gamelist_tidbit time_70\"]" document)))
+               (completionist
+                (xpath:string-value (xpath:evaluate
+                                     "//*[@class=\"gamelist_tidbit time_60\"]" document)))
+               (combined
+                (xpath:string-value (xpath:evaluate
+                                     "//*[@class=\"gamelist_tidbit time_100\"]" document))))
+          (format t "======================~C~
+                     Game Name: ~A~%~
+                     Main Story: ~A~%~
+                     Main + Extra: ~A~%~
+                     Completionist: ~A~%~
+                     Combined: ~A~%"
+                  #\newline gamename mainstory mainplusextra completionist combined)))))
+
+(defun main()
+  (if (= (length *posix-argv*) 2)
+      (fetch-and-print (nth 1 *posix-argv*))
+      (print-usage)))
+
 
 ;;; Thou shall compile!
 (save-lisp-and-die "cl-hltb" :toplevel #'main :executable t)
